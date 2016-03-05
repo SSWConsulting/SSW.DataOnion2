d15039e92af6ac14c49d6bc15ca4be4fe5aafbc4
igor.goldobin@gmail.com <Phoenix88>
2016-03-05 13:01:00 +1000

    Added microsoft DI for dataonion

---
+++ EF7/SSW.DataOnion/src/SSW.DataOnion.DependencyResolution.Microsoft/DataModule.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using SSW.DataOnion.Core;
+using SSW.DataOnion.Core.Initializers;
+using SSW.DataOnion.Interfaces;
+
+namespace SSW.DataOnion.DependencyResolution.Microsoft
+{
+    public static class DataOnionExtensions
+    {
+        public static IServiceCollection AddDataOnion(this IServiceCollection serviceCollection, string connectionString, IDatabaseInitializer databaseInitializer = null)
+        {
+            serviceCollection.AddTransient<IDbContextScope, DbContextScope>();
+            serviceCollection.AddTransient<IDbContextReadOnlyScope, DbContextReadOnlyScope>();
+            serviceCollection.AddTransient<IRepositoryResolver, MicrosoftRepositoryResolver>();
+            serviceCollection.AddScoped<IDbContextScopeFactory, DbContextScopeFactory>();
+            serviceCollection.AddScoped<IRepositoryLocator, RepositoryLocator>();
+            serviceCollection.AddTransient<IAmbientDbContextLocator, AmbientDbContextLocator>();
+            serviceCollection.AddTransient<IUnitOfWork, UnitOfWork>();
+            serviceCollection.AddTransient<IReadOnlyUnitOfWork, ReadOnlyUnitOfWork>();
+
+            serviceCollection.AddTransient<IDbContextReadOnlyScope, DbContextReadOnlyScope>();
+            serviceCollection.AddTransient<IDbContextFactory>(
+                container => new DbContextFactory(connectionString, container.GetService<IDatabaseInitializer>()));
+
+            if (databaseInitializer == null)
+            {
+                serviceCollection.AddTransient<IDatabaseInitializer, MigrateToLatestVersion>();
+            }
+            else
+            {
+                serviceCollection.AddTransient<IDatabaseInitializer, MigrateToLatestVersion>();
+                serviceCollection.AddTransient(container => databaseInitializer);
+            }
+
+            return serviceCollection;
+        }
+    }
+}
