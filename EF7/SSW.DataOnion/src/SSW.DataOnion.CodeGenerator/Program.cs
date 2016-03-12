e6ecf4b984d10db5077c350165bc1e85226b0e20
Igor Goldobin <igor.goldobin@gmail.com>
2016-03-12 13:24:11 +1000

    Added arguments parser for code gen

--- EF7/SSW.DataOnion/src/SSW.DataOnion.CodeGenerator/Program.cs
+++ EF7/SSW.DataOnion/src/SSW.DataOnion.CodeGenerator/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.Extensions.PlatformAbstractions;
 using Serilog;
-using System.Reflection;
-using System.Text;
+using Fclp;
+using SSW.DataOnion.CodeGenerator.Exceptions;
 using SSW.DataOnion.CodeGenerator.Helpers;
 
 namespace SSW.DataOnion.CodeGenerator
@@ -15,14 +10,80 @@ namespace SSW.DataOnion.CodeGenerator
     {
         public static void Main(string[] args)
         {
-            var app = PlatformServices.Default.Application;
-            var log = new LoggerConfiguration().WriteTo.ColoredConsole().CreateLogger();
-            Log.Logger = log;
-            log.Information("Hello world");
+            var logger = new LoggerConfiguration().WriteTo.ColoredConsole().CreateLogger();
+            Log.Logger = logger;
+            
+            logger.Information("Parsing options");
+            var parser = CreateArgumentParser(logger);
+            var parseResult = parser.Parse(args);
 
-            var dbContextTemplate = ResourceReader.GetResourceContents("DbContext.template");
-            log.Information(dbContextTemplate);
-            Console.ReadLine();
+            if (parseResult.HelpCalled)
+            {
+                return;
+            }
+
+            if (parseResult.HasErrors)
+            {
+                logger.Warning("Some required options are missing (see below). Please use '-?', '/?', '/help' or '--help' parameter to get list of options.");
+                foreach (var error in parseResult.Errors)
+                {
+                    logger.Error(
+                        $"'{error.Option.Description}' was not found.");
+                }
+            }
+            else
+            {
+                logger.Information("Options successfully parsed. Generating DbContext");
+                var parsedArguments = parser.Object;
+                var generator = new DbContextGenerator();
+                try
+                {
+                    generator.Generate(parsedArguments.DbContextName, parsedArguments.EntitiesNamespace,
+                        parsedArguments.DataNamespace, parsedArguments.EntitiesDll, parsedArguments.EntityBaseClass);
+                }
+                catch (GenerationException exception)
+                {
+                    logger.Error(exception, exception.Message);
+                }
+                catch (Exception exception)
+                {
+                    logger.Fatal(exception, "Unknow error occured. Details: {error}", exception.Message);
+                }
+            }
+        }
+
+        private static FluentCommandLineParser<ApplicationArguments> CreateArgumentParser(ILogger logger)
+        {
+            logger.Debug($"Create a generic parser for the '{nameof(ApplicationArguments)}' type");
+            var parser = new FluentCommandLineParser<ApplicationArguments>();
+
+            logger.Debug("Setup argument options");
+
+            parser.Setup(arg => arg.EntitiesNamespace)
+                .As("entitiesNamespace")
+                .Required()
+                .WithDescription("Namespace for domain entities. For multiple namespaces use comma separated string.");
+
+            parser.Setup(arg => arg.DataNamespace)
+                .As("dataNamespace")
+                .Required()
+                .WithDescription("\tNamespace for you data project, where DbContext resides.");
+
+            parser.Setup(arg => arg.EntitiesDll)
+                .As("entitiesDll")
+                .Required()
+                .WithDescription("\t\tFile path to the 'dll' file containing domain entities.");
+
+            parser.Setup(arg => arg.DbContextName).As("name").WithDescription("\t\t\tName of your DbContext");
+
+            parser.Setup(arg => arg.EntityBaseClass)
+                .As("batch")
+                .SetDefault(null)
+                .WithDescription(
+                    "\t\t\tOptional name of the base class entity if such exists. Only entites that use this base class will be used.");
+
+            parser.SetupHelp("?", "help").Callback(text => logger.Information(text));
+            return parser;
         }
     }
 }
